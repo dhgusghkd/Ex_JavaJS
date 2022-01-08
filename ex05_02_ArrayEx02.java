f3550b77693360de694173b6770cb0d3d79f61f1
dhgusghkd <90690779+dhgusghkd@users.noreply.github.com>
2022-01-08 22:06:32 +0900

    자바의 정석 예제 5-2
    연습중

---
+++ ex05_02_ArrayEx02.java
@@ -0,0 +1,30 @@
+package ex05;
+
+import java.util.Arrays;
+
+public class ex05_02_ArrayEx02 {
+	public static void main(String[] args) {
+		
+		int[] iArr1 = new int[10];
+		int[] iArr2 = new int[10];
+		//int[] iArr3 = new int[] {100,95,80,70,60};
+		int[] iArr3 = {100,95,80,70,60};
+		char[] chArr = {'a','b','c','d'};
+		
+		for(int i=0; i<iArr1.length; i++) {
+			iArr1[i] = i+1; //1~10의 숫자를 순서대로 배열에 넣는다.
+		}
+		for(int i=0; i<iArr2.length; i++) {
+			iArr2[i] = (int) (Math.random()*10) +1; //1~10의 값을 배열에 저장	
+		}
+		for(int i =0; i<iArr1.length; i++) {
+			System.out.print(iArr1[i]+","); //1,2,3,4,5,6,7,8,9,10,
+		}
+		System.out.println();
+		System.out.println(Arrays.toString(iArr2));//[9, 5, 10, 2, 3, 3, 10, 5, 5, 8]
+		System.out.println(Arrays.toString(iArr3));//[100, 95, 80, 70, 60]
+		System.out.println(Arrays.toString(chArr));//[a, b, c, d]
+		System.out.println(iArr3); //[I@36aa7bc2
+		System.out.println(chArr); //abcd
+	}
+}
