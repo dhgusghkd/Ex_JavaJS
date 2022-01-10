74571473ac9fe31fdfaa96dcbded14d7c080e901
dhgusghkd <90690779+dhgusghkd@users.noreply.github.com>
2022-01-10 14:31:28 +0900

    자바의 정석 예제 5-6

---
+++ ex05_06_ArrayEx06.java
@@ -0,0 +1,12 @@
+package ex05;
+
+public class ex05_06_ArrayEx06 {
+	public static void main(String[] args) {
+		
+		int[] score = {79,88,91,33,100,55,95};
+		
+		int max = score[0];
+		int min = score[0];
+		
+	} 
+}
