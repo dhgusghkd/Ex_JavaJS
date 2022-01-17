a02ad95ae14b9ac05dd4eab7bf49ee4d9707cf3c
dhgusghkd <90690779+dhgusghkd@users.noreply.github.com>
2022-01-18 00:36:38 +0900

    자바의 정석 예제 5-16

---
+++ ex05_16_ArrayEx16.java
@@ -0,0 +1,11 @@
+package ex05;
+
+public class ex05_16_ArrayEx16 {
+	public static void main(String[] args) {
+
+		System.out.println("매개변수의 개수:"+ args.length);
+		for(int i=0; i<args.length; i++) {
+			System.out.println("args[" + i + "] + \""+args[i] + "\"");
+		}
+	} 
+}
